5ad4f6a387b545c6a2a5dc95f4108ea7cd160007
AhmedShaltout85 <eng.abozaher@gmail.com>
2025-03-10 11:48:21 +0200

    last-update-10-03-2025

--- src/main/java/a08r/example/pick_location_integration_w_stores/stores/controllers/PickLocationIntegrationWStoresController.java
+++ src/main/java/a08r/example/pick_location_integration_w_stores/stores/controllers/PickLocationIntegrationWStoresController.java
@@ -1,19 +1,19 @@
 package a08r.example.pick_location_integration_w_stores.stores.controllers;
 
 
-import a08r.example.pick_location_integration_w_stores.stores.models.dto.PickLocationIntegrationWStoresDTO;
 import a08r.example.pick_location_integration_w_stores.stores.services.IPickLocationIntegrationWStoresServices;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 
 @RestController
 @RequestMapping(path = "/api/v1/pick-loc-w-stores")
 @CrossOrigin
-//TODO://http://localhost:9999/pick-location/api/v1/pick-loc-w-stores
+//TODO://http://localhost:9999/pick-location-integration-w-stores/api/v1/pick-loc-w-stores
 //TODO: we use @CrossOrigin to enable CORS for this controller (this mean no need to using https://localhost:9999)
 //TODO: https://documenter.getpostman.com/view/11963423/2sAXxQcrcV => POSTMAN DOCUMENTATION
 public class PickLocationIntegrationWStoresController {
@@ -25,9 +25,12 @@ public class PickLocationIntegrationWStoresController {
         this.iPickLocationIntegrationWStoresServices = iPickLocationIntegrationWStoresServices;
     }
 
+    //TODO: GET http://localhost:9999/pick-location-integration-w-stores/api/v1/pick-loc-w-stores/store-name/مخزن فرع البلد
     @GetMapping(path ="/store-name/{storeName}")
     public ResponseEntity<List<String>> getStoreQty(@PathVariable String storeName){
 
+        ResponseEntity<List<String>> lists = iPickLocationIntegrationWStoresServices.storeQty(storeName);
+        Objects.requireNonNull(lists.getBody()).forEach(System.out::println);
         return this.iPickLocationIntegrationWStoresServices.storeQty(storeName);
     }
 
